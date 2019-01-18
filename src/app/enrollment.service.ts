40941d029e31896a42c88d85d930bae028de847e
Torocy Ojweke <torocy45@gmail.com>
2019-01-18 10:59:44 +0300

    import Http in the service its module in the app module and inject it in the service

--- src/app/enrollment.service.ts
+++ src/app/enrollment.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+//first import HttpClient.then inject it
+import { HttpClient } from '@angular/common/http'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnrollmentService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 }
