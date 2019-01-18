dce90d9eff45a366addf2d81e1ef1df7c0ec5280
Torocy Ojweke <torocy45@gmail.com>
2019-01-18 11:12:35 +0300

    create a method to make post request

--- src/app/enrollment.service.ts
+++ src/app/enrollment.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 //first import HttpClient.then inject it
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { User } from './user'
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnrollmentService {
-
+//add an url to which data will be posted
+  _url=''
   constructor(private _http: HttpClient) { }
+
+  //create a method with an argument user of type User to make the post request
+  enroll(user : User){
+    this._http.post<any>(this._url, user)//the post request  will return the response as an observable so we need to subscribe to the observable in app component
+    
+
+  }
 }
